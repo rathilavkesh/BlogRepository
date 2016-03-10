7767565f5b733d1638bd3d4d1a544f2e3ff7718a
Lavkesh Rathi <lavkeshrathi2006@gmail.com>
2016-03-10 11:52:46 -0500

    Adding log4j

--- BlogWorkbench/BlogServices/src/main/java/com/javarock/controller/HelloWorldController.java
+++ BlogWorkbench/BlogServices/src/main/java/com/javarock/controller/HelloWorldController.java
@@ -1,13 +1,19 @@
 package com.javarock.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 	
+	private static final Logger logger = LoggerFactory.getLogger(HelloWorldController.class);
+	
 	@RequestMapping("/")
     String home() {
+		logger.debug("Hello Debug");
+		logger.info("Hello info");
         return "Hello World!";
     }
 
