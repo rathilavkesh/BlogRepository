454ade38b61ab42a1648ac1a0cd63b5505252400
Kuldeep Jadhav <kuldeep.jadhav@live.com>
2016-03-17 15:26:15 +0530

    removed unwanted reference

--- BlogWorkbench/BlogServices/src/test/java/com/blogging/service/UserServiceImplTest.java
+++ BlogWorkbench/BlogServices/src/test/java/com/blogging/service/UserServiceImplTest.java
@@ -10,26 +10,24 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.test.context.ContextConfiguration;
 
-import com.blogging.exceptions.BloggingException;
 import com.blogging.model.User;
 import com.blogging.repository.UserRepository;
 
 @RunWith(MockitoJUnitRunner.class)
 @ContextConfiguration
 public class UserServiceImplTest {
-	
+
 	@Mock
 	private UserRepository userRepository;
-	
+
 	@InjectMocks
 	private UserService userService = new UserServiceImpl();
 
 	@Test
-	public void shouldCallUserRepositoryToStoreTheUser() throws BloggingException {
+	public void shouldCallUserRepositoryToStoreTheUser() {
 		User user = new User();
 		userService.createUser(user);
 		verify(userRepository, times(1)).save(user);
 	}
-	
 
 }
