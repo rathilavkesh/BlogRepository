692548b6ff0145b2b53a68d44d8ec13e581251b3
Kuldeep Jadhav <kuldeep.jadhav@live.com>
2016-03-17 13:46:45 +0530

    Removed the unwanted files + added basic test

--- BlogWorkbench/BlogServices/src/test/java/com/blogging/repository/UserRepositoryTest.java
+++ BlogWorkbench/BlogServices/src/test/java/com/blogging/repository/UserRepositoryTest.java
@@ -1,55 +1,58 @@
 package com.blogging.repository;
 
+import java.util.Date;
+
 import javax.annotation.Resource;
-import javax.sql.DataSource;
 
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.orm.jpa.EntityScan;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.blogging.model.User;
-import com.blogging.repository.UserRepository;
+import com.blogging.model.UserDetail;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {UserRepositoryTest.Config.class})
 public class UserRepositoryTest {
 	
 	@Resource
-	UserRepository repo;
+	private UserRepository repo;
 	
 	@Test
-	public void test() {
-		Assert.assertTrue(true);
+	public void shouldSaveTheUser() {
+		User generatedUser = generateUser();
+		User save = repo.save(generatedUser);
+		Assert.assertNotNull(save);
+		Assert.assertNotNull(save.getUserId());
+		User findOne = repo.findOne(save.getUserId());
+		Assert.assertNotNull(findOne);
+		Assert.assertEquals("TestName", findOne.getFirstName());
 	}
 	
-	
+	private User generateUser() {
+		User user = new User();
+		user.setCreatedDate(new Date());
+		user.setFirstName("TestName");
+		user.setEmail("abc@abc.com");
+		user.setLastName("TestLastName");
+		user.setPassword("test");
+		
+		UserDetail details = new UserDetail();
+		details.setCity("Pune");
+		user.setUserDetail(details);
+		return user;
+	}
 	
 	@Configuration
-	@EntityScan(basePackages={"com.javarock.model"})
-	@EnableJpaRepositories(basePackages={"com.javarock.repository"})
+	@EntityScan(basePackages={"com.blogging.model"})
+	@EnableJpaRepositories(basePackages={"com.blogging.repository"})
 	@EnableAutoConfiguration
-	public static class Config {
-		
-		@Bean
-		public DataSource dataSource() {
-			EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
-			EmbeddedDatabase db = builder
-				.setType(EmbeddedDatabaseType.HSQL) //.H2 or .DERBY
-				.addScript("db/BLOG_DDL.sql")
-				.build();
-			return db;
-		}
-
-	}
+	public static class Config {}
 	
 }
