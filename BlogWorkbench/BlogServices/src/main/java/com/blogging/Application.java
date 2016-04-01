03c1eebc7adaae1da80f82ac12c7ee8760e06a55
Kuldeep Jadhav <kuldeep.jadhav@live.com>
2016-04-01 20:56:30 +0530

    trying to run IT....no success

--- BlogWorkbench/BlogServices/src/main/java/com/blogging/Application.java
+++ BlogWorkbench/BlogServices/src/main/java/com/blogging/Application.java
@@ -2,8 +2,9 @@ package com.blogging;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={EmbeddedMongoAutoConfiguration.class})
 public class Application {
 	
 	public static void main(String[] args) {
--- BlogWorkbench/BlogServices/src/test/java/com/blogging/repository/UserRepositoryIT.java
+++ BlogWorkbench/BlogServices/src/test/java/com/blogging/repository/UserRepositoryIT.java
@@ -1,27 +1,34 @@
 package com.blogging.repository;
 
-import static org.junit.Assert.fail;
-
+import java.io.IOException;
 import java.util.Date;
 
 import javax.annotation.Resource;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
-import org.springframework.boot.orm.jpa.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.mongo.MongoProperties;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.blogging.Application;
 import com.blogging.model.User;
+import com.mongodb.Mongo;
+import com.mongodb.MongoClientOptions;
+
+import de.flapdoodle.embed.mongo.MongodExecutable;
+import de.flapdoodle.embed.mongo.MongodProcess;
+import de.flapdoodle.embed.mongo.MongodStarter;
+import de.flapdoodle.embed.mongo.config.IMongodConfig;
+import de.flapdoodle.embed.mongo.config.MongodConfigBuilder;
+import de.flapdoodle.embed.mongo.config.Net;
+import de.flapdoodle.embed.mongo.distribution.Version;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {UserRepositoryIT.Config.class})
+@SpringApplicationConfiguration(classes = {Application.class, UserRepositoryIT.Config.class})
 public class UserRepositoryIT {
 	
 	@Resource
@@ -46,11 +53,43 @@ public class UserRepositoryIT {
 	
 	
 	@Configuration
-	@EnableMongoRepositories(basePackages={"com.blogging.repository"})
-	@EntityScan(basePackages={"com.blogging.model"})
-	@ImportResource(locations={"classpath:META-INF/spring-data-embedded-mongo.xml"})
-	@EnableAutoConfiguration(exclude={EmbeddedMongoAutoConfiguration.class})
+//	@EnableMongoRepositories(basePackages={"com.blogging.repository"})
+//	@EntityScan(basePackages={"com.blogging.model"})
+//	@EnableAutoConfiguration(exclude={EmbeddedMongoAutoConfiguration.class})
 	public static class Config {
+		
+		private static final MongodStarter starter = MongodStarter.getDefaultInstance();
+
+	    @Autowired
+	    private MongoProperties properties;
+
+	    @Autowired(required = false)
+	    private MongoClientOptions options;
+
+	    @Bean(destroyMethod = "close")
+	    public Mongo mongo() throws IOException {
+	        Net net = mongod().getConfig().net();
+	        System.out.println("host is "+ net.getServerAddress().getHostName());
+	        properties.setHost(net.getServerAddress().getHostName());
+	        properties.setPort(net.getPort());
+	        return properties.createMongoClient(this.options);
+	    }
+
+	    @Bean(destroyMethod = "stop")
+	    public MongodProcess mongod() throws IOException {
+	        return mongodExe().start();
+	    }
+
+	    @Bean(destroyMethod = "stop")
+	    public MongodExecutable mongodExe() throws IOException {
+	        return starter.prepare(mongodConfig());
+	    }
+
+	    @Bean
+	    public IMongodConfig mongodConfig() throws IOException {
+	        return new MongodConfigBuilder().version(Version.Main.PRODUCTION).build();
+	    }
+
 		
 	}
 
