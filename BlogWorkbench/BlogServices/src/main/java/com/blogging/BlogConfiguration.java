17e50fa839602f1c199f31a68e880ff56b22ea7f
Lavkesh Rathi <lavkeshrathi2006@gmail.com>
2016-04-06 22:28:08 -0400

    Removed unwanted dependencies

--- BlogWorkbench/BlogServices/src/main/java/com/blogging/BlogConfiguration.java
+++ BlogWorkbench/BlogServices/src/main/java/com/blogging/BlogConfiguration.java
@@ -1,7 +1,6 @@
 package com.blogging;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
@@ -11,7 +10,6 @@ import com.blogging.validator.ValidatingMongoEventListener;
 
 @Configuration
 @EnableMongoRepositories(basePackages = { "com.blogging.repository" })
-@EntityScan(basePackages = { "com.blogging.model" })
 @EnableAutoConfiguration
 public class BlogConfiguration {
 
