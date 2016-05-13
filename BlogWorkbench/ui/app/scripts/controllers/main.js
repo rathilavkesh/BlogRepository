6749f7095f6c9c584b014d50773af53930e5d90c
Kuldeep Jadhav <kuldeep.jadhav@live.com>
2016-05-13 17:55:39 +0530

    adding the basic add page

--- BlogWorkbench/ui/app/scripts/controllers/main.js
+++ BlogWorkbench/ui/app/scripts/controllers/main.js
@@ -1,17 +1,18 @@
 'use strict';
 
 angular.module('todoApp')
-	.controller('MainCtrl', ['$mdSidenav', '$mdBottomSheet', '$timeout', '$log', function($mdSidenav, $mdBottomSheet, $timeout, $log) {
+	.controller('MainCtrl', ['$mdSidenav', '$mdDialog', '$timeout', '$log', function($mdSidenav, $mdDialog, $timeout, $log) {
 
 
 		var self = this;
-		self.selected="";
+		self.selected = "";
 		self.subjects = [{
 			'name': 'Java'
 		}, {
 			'name': 'Javascript'
 		}];
 		self.toggleMenu = toggleMenu;
+		self.openAddBlogPage = openAddBlogPage;
 
 		// *********************************
 		// Internal methods
@@ -36,6 +37,35 @@ angular.module('todoApp')
 			self.selected = angular.isNumber(user) ? $scope.users[user] : user;
 		}
 
+
+		function openAddBlogPage(ev) {
+			$mdDialog.show({
+					controller: DialogController,
+					templateUrl: 'views/add.html',
+					parent: angular.element(document.body),
+					targetEvent: ev,
+					clickOutsideToClose: true
+				})
+				.then(function(answer) {
+					$scope.status = 'You said the information was "' + answer + '".';
+				}, function() {
+					$scope.status = 'You cancelled the dialog.';
+				});
+			$scope.$watch(function() {
+				return $mdMedia('xs') || $mdMedia('sm');
+			}, function(wantsFullScreen) {
+				$scope.customFullscreen = (wantsFullScreen === true);
+			});
+		}
+
+		function DialogController($scope, $mdDialog) {
+			$scope.hide = function() {
+				$mdDialog.hide();
+			};
+			
+			$scope.submit = function(answer) {
+				$mdDialog.hide();
+			};
+		}
+
 	}]);
-
-
